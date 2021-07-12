c0e7cad6785b9945248edad709d480ecca3abfff
Ryan Hartadi <Ryanhartadi06@gmail.com>
2021-07-12 18:32:59 +0700

    revisi eloquent

--- database/migrations/2021_07_12_105404_create_transactions_table.php
+++ database/migrations/2021_07_12_105404_create_transactions_table.php
@@ -19,7 +19,7 @@ class CreateTransactionsTable extends Migration
             $table->integer('inscurance_price');
             $table->integer('shipping_price');
             $table->integer('total_price');
-            $table->string('transaction_status'); //UNPAID PENDING SHIPPING SUCCESS
+            $table->string('transaction_status'); //UNPAID PENDING SUCCESS FAILED
             $table->string('resi');
             $table->softDeletes();
             $table->timestamps();
